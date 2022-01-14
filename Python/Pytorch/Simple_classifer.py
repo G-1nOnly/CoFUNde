f20f727a45f66d1c5ff8640313a595ff3609b60e
Jacob Gao <64891841+G-1nOnly@users.noreply.github.com>
2022-01-14 12:08:33 +0800

    A simple classifer working on CIFAR10

---
+++ Python/Pytorch/Simple_classifer.py
@@ -0,0 +1,112 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+
+# Preprocessing from CIFAR10
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+# num-workers could be set to multi-thread if using main function
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=True, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           ' dog', 'frog', ' horse', 'ship', 'truck')
+
+
+# Simple imshow from pytorch tutorial
+def imshow(img):
+    img = img/2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        # flatten all dimensions except batch dimension
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+net.zero_grad()
+
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss = running_loss + loss.item()
+        if i % 2000 == 1999:
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+print("Finished training!")
+
+PATH = './simple_net.pth'
+torch.save(net.state_dict(), PATH)
+
+# Testing
+net = Net()
+net.load_state_dict(torch.load(PATH))
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        input_t, labels = data
+        output_t = net(input_t)
+        _, predicted = torch.max(output_t.data, 1)
+        total = total + labels.size(0)
+        correct = correct + (predicted == labels).sum().item()
+
+print(
+    f'Accuracy of the network on testing data: {(100 * correct / total):3f} %')
+
+# Random image
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}' for j in range(4)))
